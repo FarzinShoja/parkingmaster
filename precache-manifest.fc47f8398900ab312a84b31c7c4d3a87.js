9b6f22dbd50020ffcb42bc7e093ae74e39d888b7
FarzinShoja <Farzin.Valizadeh@gmail.com>
2019-11-15 11:23:40 -0500

    Updates

---
+++ precache-manifest.fc47f8398900ab312a84b31c7c4d3a87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "964cfd0af71e08dc638d5e6ba41b49cb",
+    "url": "/parkingmaster/index.html"
+  },
+  {
+    "revision": "5529e856da77e04af3e2",
+    "url": "/parkingmaster/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "ef06669171cfc4f2f472",
+    "url": "/parkingmaster/static/css/main.0790075c.chunk.css"
+  },
+  {
+    "revision": "5529e856da77e04af3e2",
+    "url": "/parkingmaster/static/js/2.d1d73cc6.chunk.js"
+  },
+  {
+    "revision": "ef06669171cfc4f2f472",
+    "url": "/parkingmaster/static/js/main.bb389139.chunk.js"
+  },
+  {
+    "revision": "18fe3db65c3ea97e3144",
+    "url": "/parkingmaster/static/js/runtime-main.6e08a334.js"
+  }
+]);
